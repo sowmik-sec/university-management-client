6d64269ee2751c632cf184d41a6a0535ec450a76
sowmik-sec <sowmik.sec@gmail.com>
2024-12-17 20:39:08 +0600

    centralised array for routes and sidebar items

---
+++ src/pages/admin/CreateAdmin.tsx
@@ -0,0 +1,5 @@
+const CreateAdmin = () => {
+  return <div>create admin</div>;
+};
+
+export default CreateAdmin;
---
+++ src/routes/admin.routes.tsx
@@ -0,0 +1,61 @@
+import AdminDashboard from "../pages/admin/AdminDashboard";
+import CreateAdmin from "../pages/admin/CreateAdmin";
+import CreateFaculty from "../pages/admin/CreateFaculty";
+import CreateStudent from "../pages/admin/CreateStudent";
+
+const adminPaths2 = [
+  {
+    name: "Dashboard",
+    path: "/admin/dashboard",
+    element: <AdminDashboard />,
+  },
+  {
+    name: "User Management",
+    children: [
+      {
+        name: "Create Admin",
+        path: "/admin/create-admin",
+        element: <CreateAdmin />,
+      },
+      {
+        name: "Create Faculty",
+        path: "/admin/create-faculty",
+        element: <CreateFaculty />,
+      },
+      {
+        name: "Create Student",
+        path: "/admin/create-student",
+        element: <CreateStudent />,
+      },
+    ],
+  },
+  {
+    name: "Course Management",
+    children: [
+      {
+        name: "Offered Course",
+        path: "/admin/offered-course",
+        element: <CreateAdmin />,
+      },
+    ],
+  },
+];
+
+export const adminPaths = [
+  {
+    path: "dashboard",
+    element: <AdminDashboard />,
+  },
+  {
+    path: "create-student",
+    element: <CreateStudent />,
+  },
+  {
+    path: "create-admin",
+    element: <CreateAdmin />,
+  },
+  {
+    path: "create-faculty",
+    element: <CreateFaculty />,
+  },
+];
