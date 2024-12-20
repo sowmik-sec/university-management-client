955fa34f26d066db2b76c28483359d7f00079380
sowmik-sec <sowmik.sec@gmail.com>
2024-12-20 22:44:34 +0600

    Render sidebar based on role

---
+++ src/utils/sidebarItemsGenerator.tsx
@@ -0,0 +1,32 @@
+import { TSidebarItem, TUserPath } from "../types";
+import { NavLink } from "react-router-dom";
+
+// type TSidebarItem = {
+//   key: string;
+//   label: ReactNode;
+//   children?: TSidebarItem[];
+// };
+export const sidebarItemsGenerator = (items: TUserPath[], role: string) => {
+  const sidebarItems = items.reduce((acc: TSidebarItem[], item) => {
+    if (item.path && item.name) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/${role}/${item.path}`}>{item.name}</NavLink>,
+      });
+    }
+
+    if (item.children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.children.map((child) => ({
+          key: child.name,
+          label: <NavLink to={`/${role}/${child.path}`}>{child.name}</NavLink>,
+        })),
+      });
+    }
+
+    return acc;
+  }, []);
+  return sidebarItems;
+};
